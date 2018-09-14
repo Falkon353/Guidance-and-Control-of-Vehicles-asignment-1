2930fbda1e2854694cf3885a461cd8f1326ad244
Martin Mostad <martimos@ntnu.no>
2018-09-14 18:48:55 +0200

    Fixed the simulations so we have the stauration on the input. Also added so the script automaticlay plots all that is needed for task 2.4

--- Oppgave2/Init.m
+++ Oppgave2/Init.m
@@ -6,6 +6,52 @@ U = 5;
 b = 0.001;
 T = 20;
 K = 0.1;
-K_p = 2;
-K_d = 80;
-K_i = 0.5;
+K_p = 1;
+K_d = 40;
+K_i = 0;%0.00001;
+
+sim('shipmoodel.slx')
+
+figure (1); clf;
+hold on;
+plot(Controll_input)
+hold off;
+grid on;
+legend('roder input');
+title('Controll input');
+xlabel('time [s]'); 
+ylabel('angle [deg]');
+
+figure (2); clf;
+hold on;
+plot(yaw_result)
+hold off;
+grid on;
+legend('Yaw');
+title('Yaw angel');
+xlabel('time [s]'); 
+ylabel('angle [deg]');
+
+figure (3); clf;
+hold on;
+plot(yaw_rate)
+hold off;
+grid on;
+legend('Yaw rate');
+title('Yaw rate');
+xlabel('time [s]'); 
+ylabel('Angular velocities [deg/s]');
+
+figure (4); clf;
+hold on;
+plot(Y_result.Data, X_result.Data);
+hold off;
+grid on;
+legend('North East position');
+title('North East position');
+xlabel('East [m]'); 
+ylabel('North [m]');
+
+
+
+
